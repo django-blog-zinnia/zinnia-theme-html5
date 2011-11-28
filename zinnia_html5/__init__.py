c80dee66347463a851b3227cdeb4adb2690b12c9
Fantomas42 <fantomas42@gmail.com>
2011-11-28 12:35:57 +0100

    fix missing quote

--- zinnia_html5/__init__.py
+++ zinnia_html5/__init__.py
@@ -5,4 +5,4 @@ __license__ = 'BSD License'
 __author__ = 'Fantomas42'
 __email__ = 'fantomas42@gmail.com'
 
-__url__ = 'https://github.com/Fantomas42/zinnia-theme-html5
+__url__ = 'https://github.com/Fantomas42/zinnia-theme-html5'
